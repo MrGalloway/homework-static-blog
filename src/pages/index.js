323c4f0750921eca5859283d711e1e15fe82fb18
throwaway667 <f18.logx@gmail.com>
2018-09-08 21:13:29 +0200

    init and title done

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { withSiteData } from 'react-static'
+//
+import logoImg from '../logo.png'
+
+export default withSiteData(({title}) => (
+  
+  <div>
+    <h1 style={{ textAlign: 'center' }}>Welcome to {title}</h1>
+    <img src={logoImg} alt="" style={{ display: 'block', margin: '0 auto' }} />
+  </div>
+))
